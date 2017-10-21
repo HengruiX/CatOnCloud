558ad4c35355c0aea34d53f872e4553e812138fc
HenryX <hx2209@columbia.edu>
2017-10-21 11:12:10 -0400

    clean up v1

--- CatOnCloudServer/db/migrate/20171021143834_remove_subscribes_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSubscribesFromUser < ActiveRecord::Migration[5.1]
-  def change
-  	remove_column :users, :subscribes, :integer
-  end
-end
--- CatOnCloudServer/db/migrate/20171021143954_add_sub_list_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddSubListToUser < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :users, :sublist, :text
-  end
-end
